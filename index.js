d283e27a92a8d9aba4e20ffedda01de3b4bc8ed4
Joy Renee Mugo <mugojoy24@gmail.com>
2024-03-25 13:02:18 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push('Ralph')
+    return cats
+}
+
+function destructivelyPrependCat(Bob){
+    cats.unshift('Bob')
+    return cats
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+    return cats
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+    return cats
+}
+
+function appendCat(Broom){
+    return [... cats, Broom]
+}
+
+function prependCat(Arnold){
+    return [Arnold, ...cats]
+}
+
+function removeLastCat(){
+   return cats.slice(0, 2)
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
